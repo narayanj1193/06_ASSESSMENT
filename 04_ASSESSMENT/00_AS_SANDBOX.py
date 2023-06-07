6dd4d0aac9c73e018d979efd9bba9e2759f80970
narayanj1193 <101072172+narayanj1193@users.noreply.github.com>
2023-06-08 11:05:26 +1200

    Add files via upload

---
+++ 04_ASSESSMENT/00_AS_SANDBOX.py
@@ -0,0 +1,30 @@
+import random
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import random
+#
+#
+# def plot_parabola(b, c):
+#     a = 1
+#
+#     x_parabola = np.linspace(-25, 25, 1000)
+#     y_parabola = a * x_parabola**2 + b * x_parabola + c
+#
+#     plt.plot(x_parabola, y_parabola, label='Parabola')
+#     plt.axhline(color='black')  # Add a horizontal line at y=0
+#     plt.axvline(color='black')
+#     plt.xlabel('x')
+#     plt.ylabel('y')
+#     plt.title('Parabola')
+#     plt.ylim(-25, 20)
+#     plt.xlim(-20, 20)
+#     plt.legend()
+#
+#     # Display the plot
+#     plt.show()
+#
+#
+# plot_parabola(random.randint(-5, 10), random.randint(-5, 10))
+#
+#
+
